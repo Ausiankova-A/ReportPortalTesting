cb9795c7c52cf87cfe9067209d60fd680c077f19
Anastasiya Ausiankova <anastasiya_ausiankova@epam.com>
2025-05-30 00:54:29 +0200

    Task_7.2

--- page_objects/LoginPage.ts
+++ page_objects/LoginPage.ts
@@ -25,7 +25,8 @@ export class LoginPage extends BasePage {
         return this.locatorAdapter.getLocator('input[name=password]');
     }
     get loginButton() {
-        return this.locatorAdapter.getLocator('button[type=submit]');
+        // return this.locatorAdapter.getLocator('button[type=submit]');
+        return this.locatorAdapter.getLocator('.loginForm__login-button-container--KT9g6 button');
     }
 
     async login() {
